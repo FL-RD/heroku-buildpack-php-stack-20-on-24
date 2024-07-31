3c05d4cd177af7a9b81f3d088692a42008c46813
Gypsy-H-Takumi <42290579+Gypsy-H-Takumi@users.noreply.github.com>
2024-08-01 06:40:45 +0900

    Update platform.php

--- bin/util/platform.php
+++ bin/util/platform.php
@@ -258,7 +258,7 @@ $require["heroku-sys/nginx"] = "^1.8.0";
 
 preg_match("#^([^-]+)(?:-([0-9]+))?\$#", $STACK, $stack);
 $provide = ["heroku-sys/" . $stack[1] => ((isset($stack[2]) ? $stack[2] : "1") . gmdate(".Y.m.d"))]; # heroku: 20.2021.02.04 etc
-array_push($provide, ["heroku-sys/heroku" => "20.0.0"]);
+$provide["heroku-sys/heroku"] = "20.0.0";
 
 $replace = [];
 // check whether the blackfire CLI is already there (from their https://github.com/blackfireio/integration-heroku buildpack)
